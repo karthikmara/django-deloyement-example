7e06c3dbc4001d5d3da4a44bc3b9b26f83afa799
karthikmara <mkcool2010@gmail.com>
2020-01-05 10:40:56 +0530

    'first'

---
+++ First_Django_Project/Hello_app/admin.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from Hello_app.models import Topic,webpage,AccessRecord
+# Register your models here.
+
+
+admin.site.register(Topic)
+admin.site.register(webpage)
+admin.site.register(AccessRecord)
---
+++ First_Django_Project/populate_Hello_app.py
@@ -0,0 +1,48 @@
+import os
+os.environ.setdefault('DJANGO_SETTINGS_MODULE','First_Django_Project.settings')
+
+
+import django
+django.setup()
+
+import random
+from Hello_app.models import AccessRecord,Topic,webpage
+from faker import Faker
+
+fakegen = Faker()
+topics = [
+    'Search',
+    'Social',
+    'Marketplace',
+    'News',
+    'Games'
+]
+
+def add_topic():
+    t=Topic.objects.get_or_create(top_name=random.choice(topics))[0]
+    t.save()
+    return t
+
+
+
+def populate(N=5):
+
+    for entry in range(N):
+        top = add_topic()
+
+        fake_url = fakegen.url()
+        fake_date = fakegen.date()
+        fake_name = fakegen.company()
+
+
+        webpg = webpage.objects.get_or_create(topic=top,url=fake_url,name=fake_name)[0]
+
+        acc_rec = AccessRecord.objects.get_or_create(name=webpg,date=fake_date)[0]
+
+
+
+
+if __name__ =='__main__':
+    print("Populating script")
+    populate(20)
+    print("Populatingcomplete")
